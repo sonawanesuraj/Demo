c0b9a57b309efda780a46cd2501a1635f4458e2d
Suraj <sonawanesuraj5552@gmail.com>
2021-08-28 14:08:22 +0530

    first commit index file added

---
+++ AllinOneStatusSaver/AllinOneStatusSaver/app/src/main/java/com/mobikode/statussaver/interfaces/UserListInterface.java
@@ -0,0 +1,8 @@
+package com.mobikode.statussaver.interfaces;
+
+
+import com.mobikode.statussaver.model.story.TrayModel;
+
+public interface UserListInterface {
+    void userListClick(int position, TrayModel trayModel);
+}
---
+++ WhatsApp_Status_Saver-4.6/WhatsApp_Status_Saver-4.6/app/src/main/java/a/gautham/statusdownloader/Adapter/PageAdapter.java
@@ -0,0 +1,37 @@
+package a.gautham.statusdownloader.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import a.gautham.statusdownloader.Fragments.ImageFragment;
+import a.gautham.statusdownloader.Fragments.SavedFilesFragment;
+import a.gautham.statusdownloader.Fragments.VideoFragment;
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    private final int totalTabs;
+
+    public PageAdapter(@NonNull FragmentManager fm, int totalTabs) {
+        super(fm);
+        this.totalTabs = totalTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        if (position == 1) {
+            return new VideoFragment();
+        }else if (position == 2)
+            return new SavedFilesFragment();
+        return new ImageFragment();
+
+    }
+
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+}
